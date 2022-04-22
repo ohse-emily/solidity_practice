9456ede1b7413df80c18afde6661898988455d00
Emily <saeee210@gmail.com>
2022-04-22 21:43:56 +0900

    if used

---
+++ contract.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.16;
+
+contract GeneralWalletCompatibleToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    mapping (address => uint256) public balanceOf;
+
+    event Transfer(address _from, address _to, uint8 _value);
+
+    function UnsecureGeneralWalletCompatibleToken(string tokenName, string tokenSymbol, uint8 decimalUnits, uint256 initialSupply) public {
+        name = tokenName;
+        symbol = tokenSymbol;
+        decimals = decimalUnits;
+        balanceOf[msg.sender] = initialSupply;
+    }
+
+    function transfer (address _to, uint256 _value) public {
+        if (balanceOf[msg.sender] < _value ) revert();
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert();
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender,  _to, _value);
+    }
+
+}
+
+contract CrowdFundWithUnsecureToken {
+    UnsecureGeneralWalletCompatibleToken tokenReward;
+    uint8 transferCount public;
+
+
+    function transfer() external {
+        transferCount++;
+        tokenReward.transfer(msg.sender, 10);
+    }
+
+
+}
